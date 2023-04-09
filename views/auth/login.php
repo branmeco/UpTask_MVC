2b9cb032b7c243b63fc5d10c667016c636a872b7
branmeco <branmeco17@gmail.com>
2023-04-09 05:09:41 -0500

    Creando el formulario de login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,23 @@
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y Administra tus proyectos</p>
 
-<h1>Login</h1>
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+        <form action="/" method="POST" class="formulario">
+            <div class="campo">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" placeholder="Tu E-mail" name="email" />
+            </div>
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Tu Password" name="password" />
+            </div>
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuentas? Crea una.</a>
+            <a href="/olvide">¿Olvidaste tu password?</a>
+        </div>
+    </div> <!--.contenedor -sm-->
+</div>
