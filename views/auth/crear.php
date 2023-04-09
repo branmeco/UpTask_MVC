f7c8981d70211593af77b47226546c1850030965
branmeco <branmeco17@gmail.com>
2023-04-09 13:29:36 -0500

    Creando la vista de Crear Cuenta

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,30 @@
-<h1>Desde Crear</h1>
+<div class="contenedor crear">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php'; ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crea tu cuenta en UpTask</p>
+        <form action="/" method="POST" class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" placeholder="Tu Nombre" name="nombre" />
+            </div>
+            <div class="campo">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" placeholder="Tu E-mail" name="email" />
+            </div>
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Tu Password" name="password" />
+            </div>
+            <div class="campo">
+                <label for="passwordDos">Repetir Password</label>
+                <input type="password" id="passwordDos" placeholder="Repite tu Password" name="passwordDos" />
+            </div>
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+        <div class="acciones">
+            <a href="/">¿Ya tienes cuenta? Iniciar Sesión.</a>
+            <a href="/olvide">¿Olvidaste tu password?</a>
+        </div>
+    </div> <!--.contenedor -sm-->
+</div>
