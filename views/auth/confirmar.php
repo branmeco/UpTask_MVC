be2e1b50fffeb170284fdce5e1c17e6d9b839f28
branmeco <branmeco17@gmail.com>
2023-04-10 09:30:06 -0500

    Creando el modelo de usuarios

--- views/auth/confirmar.php
+++ views/auth/confirmar.php
@@ -2,7 +2,8 @@
     <?php include_once __DIR__ . '/../templates/nombre-sitio.php'; ?>
 
     <div class="acciones">
-        <a href="/">Iniciar Sesión</a>
-
+        <div class="contenedor-sm">
+            <a href="/">Iniciar Sesión</a>
+        </div>
     </div> <!--.contenedor -sm-->
 </div>
