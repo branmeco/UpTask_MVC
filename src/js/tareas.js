6f75d9ced9429c8b3841f402f9229ffb0232926b
branmeco <branmeco17@gmail.com>
2023-04-14 15:34:59 -0500

    Añadiendo javascript para mostrar el formulario

---
+++ src/js/tareas.js
@@ -0,0 +1,9 @@
+(function () {
+    //Botón para mostrar el modal de agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        console.log ('Mostrando form...');
+    }
+})();
